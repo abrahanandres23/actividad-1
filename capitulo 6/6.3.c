3889fa22ea84e4e04e2712a386fd0d932705a6ec
abrahanandres23 <abrahanandreshernandez@hotmail.com>
2023-10-17 23:33:32 -0400

    capitulo 6

---
+++ capitulo 6/6.3.c
@@ -0,0 +1,79 @@
+
+  clude <stdio.h>
+
+ /* Suma matrices.
+ El programa, al recibir como datos dos arreglos bidimensionales del mismo
+ tamaño, calcula la suma de ambos y la almacena en un tercer arreglo
+ bidimensional. */
+
+ const intMAX = 50;
+ /* Prototipo de funciones. */
+
+ voidLectura(int [][MAX], int, int);
+ voidSuma(int[][MAX], int[][MAX], int[][MAX], int, int);
+ void Imprime(int [][MAX], int, int);
+ voidmain(void)
+ {
+ intMA[MAX][MAX], MB[MAX][MAX], MC[MAX][MAX];
+
+/* Declaración de los tres arreglos */
+ intFIL, COL;
+ do
+ {
+ printf(”Ingrese el número de filas de los arreglos: ”);
+ scanf(”%d”, &FIL);
+ }
+ while (FIL> MAX FIL< 1);
+
+/* Se verifica que el número de filas sea correcto. */
+ do
+ {
+ printf(”Ingrese el número de columnas de los arreglos: ”);
+ scanf(”%d”, &COL);
+ }
+ while (COL > MAX COL < 1);
+
+/* Se verifica que el número de columnas sea correcto. */
+
+ printf(”\nLectura del Arreglo MA\n”);
+ Lectura(MA, FIL, COL);
+
+  printf(”\nLectura del Arreglo MB\n”);
+ Lectura(MB, FIL, COL);
+ Suma (MA, MB, MC, FIL, COL);
+ printf(”\nImpresión del Arreglo MC\n”);
+ Imprime (MC, FIL, COL);
+ }
+ voidLectura(int A[][MAX], intF, int C)
+
+ /* La función Lecturase utiliza para leer un arreglo bidimensional entero de F
+ filas y Ccolumnas. */
+ {
+ intI, J;
+ for (I=0; I<F; I++)
+ for(J=0; J<C; J++)
+ {
+ printf(”Ingrese el elemento %d %d: ”, I+1, J+1);
+ scanf(”%d”, &A[I][J]);
+ }
+ }
+ void Suma(intM1[][MAX],intM2[][MAX],intM3[][MAX], intF, int C)
+
+ /* La función Sumase utiliza para sumar los arreglos y almacenar el resultado
+ en un tercer arreglo bidimensional. */
+ {
+ intI, J;
+ for (I=0; I<F; I++)
+ for (J=0; J<C; J++)
+ M3[I][J]= M1[I][J] + M2[I][J];
+ }
+ voidImprime(int A[][MAX], intF, int C)
+
+ /* La función Imprimese utiliza para escribir un arreglo bidimensional de tipo
+ entero de Ffilas y Ccolumnas. */
+ {
+ intI, J;
+ for (I=0; I<F; I++)
+ for (J=0; J<C; J++)
+ printf(”\nElemento %d %d: %d ”, I, J, A[I][J]);
+ }
